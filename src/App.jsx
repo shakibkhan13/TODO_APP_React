071ce5ae926a8383baa3395aafb4b8a450e128b5
Mdshakib12-sourov <mdshakibkhansourov4@gmail.com>
2024-09-26 14:07:38 +0600

    Shakib_dev

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Home from './Components/Home'; // Ensure the file path is correct
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+import {v4 as uuidv4} from "uuid"
+
+import Todos from './Todos'; 
+import NewTodo from './NewTodo';
+import style from './home.module.css'
+
+
+
+const Home = () => {
+
+  const  [todos , setTodos] = useState([]); 
+
+  const handleAddTodo = (todo) =>{
+    setTodos((prevTodos) => {
+      return [...prevTodos, {id:uuidv4(),  todo}]
+    });
+  }; 
+
+  const handleRemoveTodo = (id) => {
+    setTodos((prevTodos)=>{
+      const filteredTodos = prevTodos.filter((todo) => todo.id !== id); 
+      return filteredTodos ;
+    }) ; 
+  };
+
+  return (
+    <div className={style.container}>
+      <h1 style={{color : "white"}}>Todo App</h1>
+      <NewTodo  onAddTodo={handleAddTodo}/>
+      <Todos todos={todos} onRemoveTodo= {handleRemoveTodo}/>
+    </div>
+  )
+}
+
+export default Home;
